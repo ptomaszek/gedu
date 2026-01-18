4bcbf43d3661ae59a81017091991780601855a21
Pawel Tomaszek <ptomaszek@users.noreply.github.com>
2026-01-18 10:19:53 +0100

    WIP MultiTimeInput

---
+++ src/games/clock/MultiTimeInput.jsx
@@ -0,0 +1,78 @@
+import React from 'react';
+import { Box, Typography, TextField } from '@mui/material';
+
+/**
+ * MultiTimeInput component for displaying and accepting time in various formats (HH, HH:MM, HH:MM:SS).
+ * 
+ * @param {Object} props
+ * @param {string[]} props.sections - Array of sections to show, e.g., ['hh'], ['hh', 'mm'], ['hh', 'mm', 'ss']
+ * @param {Object} props.values - Current values for each section, e.g., { hh: '12', mm: '30', ss: '05' }
+ * @param {string} props.activeSection - The section currently being edited ('hh', 'mm', or 'ss')
+ * @param {string} [props.bgcolor='white'] - Background color for the inputs
+ * @param {boolean} [props.disabled=false] - Whether the input is disabled
+ * @param {Function} [props.onSectionClick] - Callback when a section is clicked (to change activeSection)
+ */
+const MultiTimeInput = ({
+    sections = ['hh'],
+    values = { hh: '', mm: '', ss: '' },
+    activeSection = 'hh',
+    bgcolor = 'white',
+    disabled = false,
+    onSectionClick,
+}) => {
+    const renderSection = (type, index) => {
+        const isActive = activeSection === type;
+        const value = values[type] || '';
+        
+        return (
+            <React.Fragment key={type}>
+                {index > 0 && (
+                    <Typography 
+                        variant="h4" 
+                        sx={{ 
+                            fontWeight: 600, 
+                            mx: 0.5,
+                            color: disabled ? 'text.disabled' : 'text.primary'
+                        }}
+                    >
+                        :
+                    </Typography>
+                )}
+                <TextField
+                    value={value}
+                    onClick={() => onSectionClick?.(type)}
+                    disabled={disabled}
+                    inputProps={{
+                        readOnly: true,
+                        style: {
+                            textAlign: 'center',
+                            fontSize: '1.6rem',
+                            fontWeight: 600,
+                            letterSpacing: '0.1em',
+                            padding: '8px 0',
+                        },
+                    }}
+                    sx={{
+                        width: 80,
+                        '& .MuiOutlinedInput-root': {
+                            bgcolor: isActive ? '#e3f2fd' : bgcolor,
+                            transition: 'background-color 0.5s ease',
+                            '& fieldset': {
+                                borderColor: isActive ? 'primary.main' : undefined,
+                                borderWidth: isActive ? 2 : 1,
+                            },
+                        },
+                    }}
+                />
+            </React.Fragment>
+        );
+    };
+
+    return (
+        <Box display="flex" alignItems="center" justifyContent="center">
+            {sections.map((section, index) => renderSection(section, index))}
+        </Box>
+    );
+};
+
+export default MultiTimeInput;
